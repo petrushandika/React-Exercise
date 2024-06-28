2a1c6cef2cbe6349da0b757d3763ca2ad31d5d19
petrushandika <petrushandikasinaga@gmail.com>
2024-06-29 00:30:54 +0700

    Navigation Structure

--- cinemate/src/pages/MovieList.jsx
+++ cinemate/src/pages/MovieList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MovieList() {
   return (
     <main>
-      <div className="text-9xl bg-emerald-200 p-10 border-8 border-blue-700 rounded-lg m-5">MovieList</div>
+      <div className="">MovieList</div>
     </main>
   );
 }
