cfc4ea4253e1bf19a2423b4aaba7f962be80a5d5
petrushandika <petrushandikasinaga@gmail.com>
2024-06-28 20:13:48 +0700

    Setup React Router

---
+++ cinemate/src/routes/AllRoutes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
+
+export default function AllRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<MovieList />} />
+        <Route path="movie/:id" element={<MovieDetail />} />
+        <Route path="movie/popular" element={<MovieList />} />
+        <Route path="movie/top" element={<MovieList />} />
+        <Route path="movie/upcoming" element={<MovieList />} />
+        <Route path="search" element={<Search />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
