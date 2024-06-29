4ce04c4c83fea9a1f24fdaad2c5932a32685f4b2
petrushandika <petrushandikasinaga@gmail.com>
2024-06-30 00:09:51 +0700

    Finishing Project

--- cinemate/src/components/Footer.jsx
+++ cinemate/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <Link href="/" className="hover:underline">
+          <Link to="/" className="hover:underline">
             Cinemate
           </Link>
           . All Rights Reserved.
@@ -14,7 +14,7 @@ export default function Footer() {
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <Link
-              href="#"
+              to="https://www.instagram.com/petrushandika"
               target="_blank"
               className="hover:underline me-4 md:me-6"
             >
@@ -23,7 +23,7 @@ export default function Footer() {
           </li>
           <li>
             <Link
-              href="#"
+              to="https://www.linkedin.com/in/petrushandika/"
               target="_blank"
               className="hover:underline me-4 md:me-6"
             >
@@ -32,15 +32,19 @@ export default function Footer() {
           </li>
           <li>
             <Link
-              href="#"
+              to="https://open.spotify.com/user/31tfwh7e36cgity6gm7nemfh4c6a?si=1a1d3a9e641b40b7&nd=1&dlsi=c8d5ba9e6d5c4757"
               target="_blank"
               className="hover:underline me-4 md:me-6"
             >
-              Youtube
+              Spotify
             </Link>
           </li>
           <li>
-            <Link href="#" target="_blank" className="hover:underline">
+            <Link
+              to="https://github.com/petrushandika"
+              target="_blank"
+              className="hover:underline"
+            >
               Github
             </Link>
           </li>
