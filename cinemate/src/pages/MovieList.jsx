66b844d1eb299b262f8ba8bb4cddcb165817ceb9
petrushandika <petrushandikasinaga@gmail.com>
2024-06-29 09:37:26 +0700

    Custom Hook: useFetch

--- cinemate/src/pages/MovieList.jsx
+++ cinemate/src/pages/MovieList.jsx
@@ -1,25 +1,14 @@
-import { useEffect, useState } from "react";
 import { Card } from "../components";
+import useFetch from "../hooks/useFetch";
 
-export default function MovieList() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=6a084c3448ea5df2616920c4dffb6fc4"
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export default function MovieList({ apiPath }) {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          {movies.map((movie) => (
+          {movies?.map((movie) => (
             <Card key={movie.id} movie={movie} />
           ))}
         </div>
--- cinemate/src/routes/AllRoutes.jsx
+++ cinemate/src/routes/AllRoutes.jsx
@@ -5,12 +5,24 @@ export default function AllRoutes() {
   return (
     <div className="dark:bg-slate-800">
       <Routes>
-        <Route path="" element={<MovieList />} />
+        <Route
+          path=""
+          element={<MovieList apiPath="movie/now_playing" title="Home" />}
+        />
         <Route path="movie/:id" element={<MovieDetail />} />
-        <Route path="movie/popular" element={<MovieList />} />
-        <Route path="movie/top" element={<MovieList />} />
-        <Route path="movie/upcoming" element={<MovieList />} />
-        <Route path="search" element={<Search />} />
+        <Route
+          path="movies/popular"
+          element={<MovieList apiPath="movie/popular" title="Popular" />}
+        />
+        <Route
+          path="movies/top"
+          element={<MovieList apiPath="movie/top_rated" title="Top Rated" />}
+        />
+        <Route
+          path="movies/upcoming"
+          element={<MovieList apiPath="movie/upcoming" title="Upcoming" />}
+        />
+        <Route path="search" element={<Search apiPath="search/movie" />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
