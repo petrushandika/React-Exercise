fde3626ffd1efbaef4a27b5394fc80daf30b0daf
petrushandika <petrushandikasinaga@gmail.com>
2024-06-29 08:55:41 +0700

    API Fetch: Movie List

--- cinemate/src/pages/MovieList.jsx
+++ cinemate/src/pages/MovieList.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components";
 
 export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=6a084c3448ea5df2616920c4dffb6fc4"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
