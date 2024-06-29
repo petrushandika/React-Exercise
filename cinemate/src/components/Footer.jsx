c4b4b5c7515127e8c2b15c6e3e7aed3dc966739c
petrushandika <petrushandikasinaga@gmail.com>
2024-06-29 15:16:44 +0700

    Tailwind Customization

--- cinemate/src/components/Footer.jsx
+++ cinemate/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-white shadow dark:bg-gray-800">
+    <footer className="bg-white shadow dark:bg-slate-900">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
@@ -13,24 +13,36 @@ export default function Footer() {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
+            <Link
+              href="#"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+            >
               Instagram
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
+            <Link
+              href="#"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+            >
               Linkedin
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
+            <Link
+              href="#"
+              target="_blank"
+              className="hover:underline me-4 md:me-6"
+            >
               Youtube
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline">
+            <Link href="#" target="_blank" className="hover:underline">
               Github
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
--- cinemate/tailwind.config.js
+++ cinemate/tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      screens: {
+        other: { min: "340px", max: "1200px" },
+      },
+      colors: {
+        darkbg: "#1E293B",
+        blue: {
+          850: "7dd3fc",
+        },
+      },
     },
   },
   plugins: [],
